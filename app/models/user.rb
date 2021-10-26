a9a212fbfaf5385684f4a0c62fed2c4863f0a476
atsuki04 <cavu04hmp@gmail.com>
2021-10-26 07:07:46 +0000

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   attachment :profile_image
+  validates :name, uniqueness: true
   validates :name,
     length: { minimum: 2, maximum: 20 }
-  validates :introduction, presence: true, uniqueness: true,
+  validates :introduction, 
                    length: { maximum: 50 }
 end
