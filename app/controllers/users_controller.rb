a3d63930c92e30543c79237f1e8a64b2314a7b91
atsuki04 <cavu04hmp@gmail.com>
2021-10-28 08:00:26 +0000

    fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   if @user == current_user
     render :edit
   else
-    redirect_to user_path
+    redirect_to user_path(current_user)
   end
   end
 
